0ad0f5c6cd98fbf886ba49da2eb441640f7225e1
Deepak Kumar <56294275+Deepak143341@users.noreply.github.com>
2026-07-14 23:13:15 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+
+        for(auto x : nums)
+        {
+            if(st.count(x))
+            {
+                return x;
+            }
+            st.insert(x);
+        }
+    }
+};
